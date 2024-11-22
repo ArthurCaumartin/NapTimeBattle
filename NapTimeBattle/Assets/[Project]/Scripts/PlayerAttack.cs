a0c254f9443ef8f18098c4c84550e2bb60981aad
Arthur Caumartin <103814696+ArthurCaumartin@users.noreply.github.com>
2024-11-22 20:01:29 +0100

    oui

--- NapTimeBattle/Assets/[Project]/Scripts/PlayerAttack.cs
+++ NapTimeBattle/Assets/[Project]/Scripts/PlayerAttack.cs
@@ -1,20 +1,28 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerAttack : MonoBehaviour
 {
     [SerializeField] private Animator _animator;
     [SerializeField] private float _attackPerSecond = 1;
     private float _attackTime;
+    private bool _hasAttack = false;
 
     private void Update()
     {
-        _animator.SetFloat("AttackSpeed", 1 / _attackPerSecond);
+        // _animator.SetFloat("AttackSpeed", 1 / _attackPerSecond);
         _attackTime += Time.deltaTime;
-        if (_attackTime > 1 / _attackPerSecond)
+        if (_attackTime > 1 / _attackPerSecond && _hasAttack)
         {
             _attackTime = 0;
             Attack();
         }
+        _hasAttack = false;
+    }
+
+    private void OnAttack(InputValue value)
+    {
+        _hasAttack = true;
     }
 
     private void Attack()
--- NapTimeBattle/Assets/[Project]/Scripts/PlayerMovement.cs
+++ NapTimeBattle/Assets/[Project]/Scripts/PlayerMovement.cs
@@ -3,6 +3,7 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private Transform _aimContainer;
     [SerializeField] private float _speed = 5;
     [SerializeField] private float _acceleration = 10;
     private Vector2 _targetVelocity;
@@ -15,10 +16,61 @@ public class PlayerMovement : MonoBehaviour
         _moveAction = GetComponent<PlayerInput>().actions.FindAction("Move");
     }
 
-
     private void FixedUpdate()
     {
         _targetVelocity = _moveAction.ReadValue<Vector2>();
         _rb.velocity = Vector2.Lerp(_rb.velocity, _targetVelocity * _speed, Time.deltaTime * _acceleration);
+        print(_rb.velocity.Get8AxisValue());
+        // _aimContainer.right.Get8AxisValue();
+    }
+}
+
+
+
+public static class Vector2Utils
+{
+    public static Vector3 Get8AxisValue(this Vector3 value)
+    {
+        Debug.Log("Value : " + value);
+        Vector3 toReturn = Vector2.zero;
+        if (value.x < -.5f) value.x = -1;
+        if (value.x < 0f && value.x > -.5f) value.x = -.5f;
+        if (value.x < -.5f && value.x > 0) value.x = 0;
+
+        if (value.x > .5f) value.x = 1;
+        if (value.x > 0f && value.x < .5f) value.x = .5f;
+        if (value.x > .5f && value.x < 0) value.x = 0;
+
+        if (value.y < -.5f) value.y = -1;
+        if (value.y < 0f && value.y > -.5f) value.y = -.5f;
+        if (value.y < -.5f && value.y > 0) value.y = 0;
+
+        if (value.y > .5f) value.y = 1;
+        if (value.y > 0f && value.y < .5f) value.y = .5f;
+        if (value.y > .5f && value.y < 0) value.y = 0;
+        value = toReturn;
+        return value;
+    }
+
+    public static Vector2 Get8AxisValue(this Vector2 value)
+    {
+        Debug.Log(value);
+        Vector3 toReturn = Vector2.zero;
+        if (value.x < -.5f) value.x = -1;
+        if (value.x < 0f && value.x > -.5f) value.x = -.5f;
+        if (value.x < -.5f && value.x > 0) value.x = 0;
+
+        if (value.x > .5f) value.x = 1;
+        if (value.x > 0f && value.x < .5f) value.x = .5f;
+        if (value.x > .5f && value.x < 0) value.x = 0;
+
+        if (value.y < -.5f) value.y = -1;
+        if (value.y < 0f && value.y > -.5f) value.y = -.5f;
+        if (value.y < -.5f && value.y > 0) value.y = 0;
+
+        if (value.y > .5f) value.y = 1;
+        if (value.y > 0f && value.y < .5f) value.y = .5f;
+        if (value.y > .5f && value.y < 0) value.y = 0;
+        return toReturn;
     }
 }
